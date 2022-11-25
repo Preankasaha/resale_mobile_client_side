75362ab1e87137967c52ecad98db447af940138e
unknown <preansaha@gmail.com>
2022-11-25 23:10:49 +0600

    advertisement section added in home page

---
+++ src/pages/Home/Home/Advertisement/Advertisement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Advertisement = () => {
+    return (
+        <div className='bg-neutral-100'>
+            <h3 className='text-5xl text-white font-bold text-center uppercase'>Advertisement</h3>
+            <p className='text-2xl text-white  text-center my-4'>Advertise Your Producte Here</p>
+        </div>
+    );
+};
+
+export default Advertisement;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Categories from '../Categories/Categories/Categories';
-
-
-
-
+import Advertisement from './Advertisement/Advertisement';
 
 const Home = () => {
     return (
-
         <div>
-
             <Banner></Banner>
-
             <Categories></Categories>
+            <Advertisement></Advertisement>
         </div>
     );
 };
